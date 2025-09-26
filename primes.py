a07ff278d83d05246772b6a6ea74d648131e475d
Mateus Goulart <mateusgoulart25@gmail.com>
2025-09-25 21:02:05 -0300

    primos

--- primes.py
+++ primes.py
@@ -1,5 +1,6 @@
 import csv
 import time
+import random
 
 class LinearCongruentialGenerator:
     def __init__(self, seed):
@@ -30,7 +31,47 @@ class XorShiftGenerator:
 
         # self.seed |= (1 << (n_bits - 1))
         return self.seed
-        
+    
+
+class MillerRabin:
+    def __init__(self, n, k=5):
+        """
+        n: número a ser testado
+        k: número de iterações do teste, mais iterações = maior confiabilidade
+        """
+        self.n = n
+        self.k = k
+
+    def is_prime(self):
+        n = self.n
+        if n < 2:
+            return False
+        if n in (2, 3):
+            return True
+        if n % 2 == 0:
+            return False
+
+        # Escrevendo n-1 como 2^r * d
+        d = n - 1
+        r = 0
+        while d % 2 == 0:
+            d //= 2
+            r += 1
+
+        # Testes de Miller-Rabin
+        for _ in range(self.k):
+            a = random.randrange(2, n - 1)
+            x = pow(a, d, n)
+            if x == 1 or x == n - 1:
+                continue
+            for _ in range(r - 1):
+                x = pow(x, 2, n)
+                if x == n - 1:
+                    break
+            else:
+                return False
+        return True
+
 
 
 if __name__ == "__main__":
@@ -51,9 +92,13 @@ if __name__ == "__main__":
         lcg_n_bits_dict = {}
         for n in n_bits:
             avg = 0
-            for i in range(100000):
+            for i in range(1):
                 start = time.perf_counter()
                 random_number = linear.algorithm(n)
+                miller_rabin = MillerRabin(random_number)
+                while not miller_rabin.is_prime():
+                    random_number = linear.algorithm(n)
+                    miller_rabin = MillerRabin(random_number)
                 end = time.perf_counter()
                 avg += (end - start)
                 writer.writerow(["LCG", n, random_number.bit_length(), i, random_number])
@@ -71,13 +116,17 @@ if __name__ == "__main__":
         xor_n_bits_dict = {}
         for n in n_bits:
             avg = 0
-            for i in range(100000):
+            for i in range(10):
                 start = time.perf_counter()
                 random_number = xorshift.algorithm(n)
+                miller_rabin = MillerRabin(random_number)
+                while not miller_rabin.is_prime():
+                    random_number = xorshift.algorithm(n)
+                    miller_rabin = MillerRabin(random_number)
                 end = time.perf_counter()
                 avg += (end - start)
                 writer.writerow(["XORSHIFT", n, random_number.bit_length(), i, random_number])
-            
+        
             xor_n_bits_dict.update({n: f'{(avg/100000)*1000:.2f}'})
 
             print(f"n_bits: {n}, avg time: {(avg/1000000)*1000:.2f} ms")
